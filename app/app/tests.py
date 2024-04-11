24e11fdb9abe58c0e1034ae766e6fa2372fc0d0b
francisco-oro <francisco.oro@comunidad.unam.mx>
2024-04-10 18:56:48 -0600

    Added postgresql service in docker-compose.yml

--- app/app/tests.py
+++ app/app/tests.py
@@ -5,6 +5,7 @@ Sample tests
 from django.test import SimpleTestCase
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """Test the calc module."""
 
@@ -12,4 +13,8 @@ class CalcTests(SimpleTestCase):
         """Test adding numbers together."""
         res = calc.add(5, 6)
 
-        self.assertEqual(res, )
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        res = calc.subtract(10, 15)
+        self.assertEqual(res, 5)
