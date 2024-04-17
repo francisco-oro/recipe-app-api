f304b20c3e932ae40f190b3880ccca1afb8bf3bb
francisco-oro <francisco.oro@comunidad.unam.mx>
2024-04-17 10:53:59 -0600

    test_admin.py fix #2

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -6,8 +6,13 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.test import Client
 from django.db.utils import IntegrityError
+from django.db import transaction
+
+from core.models import User
 
 from psycopg2.errors import UniqueViolation
+
+from unittest.mock import patch
 
 
 class AdminSiteTests(TestCase):
@@ -17,21 +22,25 @@ class AdminSiteTests(TestCase):
         """Create user and client"""
         try:
             self.client = Client()
-            self.admin_user = get_user_model().objects.create_superuser(
-                email='admin@example.com',
-                password='testpass123'
-            )
-            self.client.force_login(self.admin_user)
-            self.user = get_user_model().objects.create_user(
-                email='user@example.com',
-                password='testpass123',
-                name='Test User'
-            )
+            with transaction.atomic():
+                self.admin_user = get_user_model().objects.create_superuser(
+                    email='admin@example.com',
+                    password='testpass123'
+                )
         except (UniqueViolation, IntegrityError):
             print("User already exists")
+        self.user = User(email='user@examp.com', password='testpass123', name='Test User')
 
-    def test_users_list(self):
+    @patch('django.test.Client.get')
+    def test_users_list(self, patched_get):
         """Test that users are listed on page."""
+        patched_get.return_value = [
+            {
+                'name': 'Test User',
+                'email': 'user@example.com'
+            }
+        ]
+
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
 
