e932a85d02a766f9783d76a88f740b1d2556c1d8
francisco-oro <francisco.oro@comunidad.unam.mx>
2024-04-17 11:40:17 -0600

    Added support to modifying users

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -7,6 +7,7 @@ from django.urls import reverse
 from django.test import Client
 from django.db.utils import IntegrityError
 from django.db import transaction
+from django.http import HttpResponse
 
 from core.models import User
 
@@ -27,22 +28,26 @@ class AdminSiteTests(TestCase):
                     email='admin@example.com',
                     password='testpass123'
                 )
+                self.user = get_user_model().objects.create_user(
+                    email='user@example.com',
+                    password='testpass123',
+                    name='Test User'
+                )
         except (UniqueViolation, IntegrityError):
             print("User already exists")
-        self.user = User(email='user@examp.com', password='testpass123', name='Test User')
+        self.user = User(email='user@example.com', password='testpass123', name='Test User')
 
-    @patch('django.test.Client.get')
-    def test_users_list(self, patched_get):
+    def test_users_lists(self):
         """Test that users are listed on page."""
-        patched_get.return_value = [
-            {
-                'name': 'Test User',
-                'email': 'user@example.com'
-            }
-        ]
-
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        """Test the edit user page works."""
+        url = reverse('admin"core_user_change')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
