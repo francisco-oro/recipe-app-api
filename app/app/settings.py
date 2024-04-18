3767d927a4994595b6eb365ae827cda0cb00f201
francisco-oro <francisco.oro@comunidad.unam.mx>
2024-04-17 23:04:21 -0600

    Added Swagger UI Support

--- app/app/settings.py
+++ app/app/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'rest_framework',
-    'drf_spectacular'
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
