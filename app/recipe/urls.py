d8a923302450fb2643f10aad2e2a1eeb7d3c3636
francisco-oro <francisco.oro@comunidad.unam.mx>
2024-04-22 18:55:17 -0600

    Written tests for creating recipes

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -9,3 +9,12 @@ from django.urls import (
 from rest_framework.routers import DefaultRouter
 
 from recipe import views
+
+router = DefaultRouter()
+router.register('recipes', views.RecipeViewSet)
+
+app_name = 'recipe'
+
+urlpatterns = [
+    path('', include(router.urls))
+]
